f05579d1b95e9e2fc93d67f46a70406f75aeb835
liuanping <2300820209@qq.com>
2020-02-09 12:16:51 +0800

    first commit

---
+++ src/view/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from 'antd'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        typescript react
+        <Button type="primary">Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
