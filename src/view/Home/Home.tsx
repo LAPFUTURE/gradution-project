6dd7d7f44564027169266354941cbe549c12a1df
liuanping <2300820209@qq.com>
2020-02-29 14:07:02 +0800

    添加npmrc

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Button } from 'antd'
 export default function Home() {
     return (
         <div>
             <h2>Home</h2>
+            <Button> yeah </Button>
         </div>
     )
 }
